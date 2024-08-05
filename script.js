a89dc32e1b71f2f96040a226a56dc94e353dc2f0
Hardik Sharma <sharmahardik606@gmail.com>
2024-08-05 14:55:04 +0530

    added enter key press to store

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ let listTab = document.querySelector('input');
 let button = document.querySelector('button');
 let ul = document.querySelector('ul');
 
+
 button.addEventListener('click', () => {
+    if (listTab.value === '') {
+        return;
+    }
     ul.innerHTML += `<li>${listTab.value}</li>`;
     listTab.value = '';
+});
+
+
+listTab.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter'){
+        if (listTab.value === '') {
+            return;
+        }
+    ul.innerHTML += `<li>${listTab.value}</li>`;
+
+    listTab.value = '';
+    }
 });

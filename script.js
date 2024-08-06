755379894982af74dfc4a1e55ccc9bde80748448
Hardik Sharma <sharmahardik606@gmail.com>
2024-08-06 12:23:05 +0530

    added more functinality

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 let listTab = document.querySelector('input');
 let button = document.querySelector('button');
 let ul = document.querySelector('ul');
+let cross = document.querySelector('.cross');
+
+cross.style.display = 'none';
+
+
+const mainFunc = function(){
+    let newLi = document.createElement('li');
+    newLi.textContent = listTab.value ;
+    ul.appendChild(newLi);
+    listTab.value = '';
+}
 
 
 button.addEventListener('click', () => {
     if (listTab.value === '') {
         return;
     }
-    ul.innerHTML += `<li>${listTab.value}</li>`;
-    listTab.value = '';
+    mainFunc();
 });
 
 
@@ -17,8 +27,23 @@ listTab.addEventListener('keydown', (event) => {
         if (listTab.value === '') {
             return;
         }
-    ul.innerHTML += `<li>${listTab.value}</li>`;
+     mainFunc();
+    }
+});
 
-    listTab.value = '';
+
+
+ul.addEventListener('click', (event) => {
+    if (event.target.tagName === 'LI') {
+        const isStrikethrough = event.target.style.textDecoration === 'line-through';
+        const isDisc = event.target.style.listStyle === 'disc';
+
+        if (isStrikethrough && isDisc) {
+            event.target.style.textDecoration = 'none';
+            event.target.style.listStyle = 'circle';
+        } else {
+            event.target.style.textDecoration = 'line-through';
+            event.target.style.listStyle = 'disc';
+        }
     }
 });
